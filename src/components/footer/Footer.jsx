d96b07994b67d480e375f97629054292271a5897
V3Solution <mubeenyasin75@gmail.com>
2023-10-30 18:13:23 +0500

    working on the nav bar

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './footer.module.css'
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      this is footer component.
+    </div>
+  )
+}
+
+export default Footer
