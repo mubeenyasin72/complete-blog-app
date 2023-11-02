123dc1f0e48d22bb009df48c308c842e84709bc7
V3Solution <mubeenyasin75@gmail.com>
2023-11-02 15:39:14 +0500

    home page card completed

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
+import Image from "next/image";
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>React Post</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="blogs" fill />
+          </div>
+          <div className={styles.textContainer}></div>
+        </div>
+      </div>
       <Pagination />
     </div>
   );
--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const CategoryList = () => {
+  const data = [
+    {
+      id:1,
+      img: "/style.png",
+      title: "Style",
+    },
+    {
+      id:2,
+      img: "/fashion.png",
+      title: "Fashion",
+    },
+    {
+      id:3,
+      img: "/food.png",
+      title: "Food",
+    },
+    {
+      id:4,
+      img: "/travel.png",
+      title: "Travel",
+    },
+    {
+      id:5,
+      img: "/culture.png",
+      title: "Culture",
+    },
+    {
+      id:6,
+      img: "/coding.png",
+      title: "Coding",
+    },
+  ];
   return (
-    <div>
-      CategoryList
+     <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {data?.map((item) => (
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
