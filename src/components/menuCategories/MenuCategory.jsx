8f137feeb50202d11c720320a172d213d57324d2
V3Solution <mubeenyasin75@gmail.com>
2023-11-11 16:17:19 +0500

    side menu is completed

--- src/components/menuCategories/MenuCategory.jsx
+++ src/components/menuCategories/MenuCategory.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
+import styles from "./menuCategory.module.css";
 const MenuCategory = () => {
   return (
-    <div>
-      MenuCategory
+    <div className={styles.categoryList}>
+      <Link
+        href="/blog?cat=style"
+        className={`${styles.categoryItem} ${styles.style}`}
+      >
+        Style
+      </Link>
+      <Link href="/blog" className={`${styles.categoryItem} ${styles.fashion}`}>
+        Fashion
+      </Link>
+      <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>
+        Food
+      </Link>
+      <Link href="/blog" className={`${styles.categoryItem} ${styles.travel}`}>
+        Travel
+      </Link>
+      <Link href="/blog" className={`${styles.categoryItem} ${styles.culture}`}>
+        Culture
+      </Link>
+      <Link href="/blog" className={`${styles.categoryItem} ${styles.coding}`}>
+        Coding
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default MenuCategory
+export default MenuCategory;
