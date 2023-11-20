f95b3b938690bbb3770d8df8c17254c8fdc54e79
V3Solution <mubeenyasin75@gmail.com>
2023-11-20 17:43:13 +0500

    pagination and footer section is completed:

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -4,17 +4,23 @@ import Pagination from "../pagination/Pagination";
 import Image from "next/image";
 import Card from "../card/Card";
 const CardList = () => {
+  const count = 2;
+  const page = 3;
+  const POST_PER_PAGE = 2;
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>React Post</h1>
       <div className={styles.posts}>
         <Card />
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
-      <Pagination />
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-const Pagination = () => {
+import React from "react";
+import styles from "./pagination.module.css";
+const Pagination = ({ page, hasPrev, hasNext }) => {
   return (
-    <div>
-      Pagination
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        // disabled={!hasPrev}
+        // onClick={() => router.push(`?page=${page - 1}`)}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+        // disabled={!hasNext}
+        // onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
