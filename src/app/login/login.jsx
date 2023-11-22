56b1169d21be2f4097cf8843f15f33d1983091ec
V3Solution <mubeenyasin75@gmail.com>
2023-11-22 16:41:52 +0500

    single page completed

---
+++ src/app/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './login.module.css'
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      LoginPage
+    </div>
+  )
+}
+
+export default LoginPage
