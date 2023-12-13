3b9f802a05ca72cd8c6e8523925f74a0086df124
V3Solution <mubeenyasin75@gmail.com>
2023-12-13 14:36:03 +0500

    working on the login page 04

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
-import styles from './login.module.css'
+import React from "react";
+import styles from "./login.module.css";
 const LoginPage = () => {
   return (
     <div className={styles.container}>
       this is the login page..
       <div className={styles.body}>
-        this is login page body...
-      this is login page body...
-        
-      this is login page body...
-
+        this is login page body... this is login page body... this is login page
+        body...
       </div>
-            this is the login page..
+      this is the login page..
       <div className={styles.body}>
-        this is login page body...
-      this is login page body...
-        
-      this is login page body...
-
+        this is login page body... this is login page body... this is login page
+        body...
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
