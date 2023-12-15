843f335cba5af27045ed1390654151b3d4d66fb5
V3Solution <mubeenyasin75@gmail.com>
2023-12-15 15:35:51 +0500

    working on the login page 10

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -13,6 +13,11 @@ const LoginPage = () => {
         body...
       </div>
       this is the login page..
+         <div className={styles.body}>
+        this is login page body... this is login page body... this is login page
+        body...
+      </div>
+      this is the login page..
     </div>
   );
 };
