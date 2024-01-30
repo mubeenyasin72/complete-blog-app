dd95abe6b0b7750d5db594fa6e4d0cfd3aeb1067
V3Solution <mubeenyasin75@gmail.com>
2024-01-30 13:35:28 +0500

    trying to fix routing issue4

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -14,7 +14,13 @@ const LoginPage = () => {
         body...
       </div>
       this is the login page..
+            <div className={styles.body}>
+        this is login page body... this is login page body... this is login page
+        body...
+      </div>
+      this is the login page..
     </div>
+    
   );
 };
 
