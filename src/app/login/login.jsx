743762ddfd659a20331d9e7562e6c88ab20b41da
V3Solution <mubeenyasin75@gmail.com>
2024-02-23 19:56:02 +0500

    issue while creating routing

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -14,11 +14,6 @@ const LoginPage = () => {
         body...
       </div>
       this is the login page..
-            <div className={styles.body}>
-        this is login page body... this is login page body... this is login page
-        body...
-      </div>
-      this is the login page..
     </div>
     
   );
